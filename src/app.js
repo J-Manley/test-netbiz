7291b703bbea96ea44bf14fb920b2fceb17f144e
Joel Manley <j_90@live.co.uk>
2022-02-27 20:12:47 +0000

    completes project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// Mobile Nav
+const nav = document.querySelector(".mobile-nav");
+const navItems = document.querySelectorAll(".mobile-nav__item");
+const toggle = document.querySelector(".mobile-toggle");
+const closeIcon = document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menu-icon");
+
+function toggleNav() {
+  if (nav.classList.contains("show-nav")) {
+    nav.classList.remove("show-nav");
+    menuIcon.style.display = "block";
+  } else {
+    nav.classList.add("show-nav");
+    closeIcon.style.display = "block";
+  }
+}
+
+toggle.addEventListener("click", toggleNav);
+closeIcon.addEventListener("click", toggleNav);
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", toggleNav);
+});
